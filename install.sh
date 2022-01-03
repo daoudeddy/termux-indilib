a24c18d873f25f5ff3f1f2ea191a1aa18e143542
daoudeddy <daoudedy@gmail.com>
2022-01-03 20:23:24 +0200

    Added indi-3rdparty support, see README

--- install.sh
+++ install.sh
@@ -13,14 +13,29 @@
 
 export PREFIX="/data/data/com.termux/files/usr"
 
-LIB_NOVA_URL="https://git.code.sf.net/p/libnova/libnova"
-LIB_NOVA_DIR="libnova"
+LIBNOVA_URL="https://git.code.sf.net/p/libnova/libnova"
+LIBNOVA_DIR="libnova"
 
 CFITSIO_URL="http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-4.0.0.tar.gz"
 CFITSIO_DIR="cfitsio-4.0.0"
 
-INDI_LIB_URL="https://github.com/indilib/indi.git"
-INDI_LIB_DIR="indi"
+LIBRAW_CMAKE_URL="https://github.com/LibRaw/LibRaw-cmake.git"
+LIBRAW_CMAKE_DIR="libraw-cmake"
+
+LIBRAW_URL="https://github.com/LibRaw/LibRaw.git"
+LIBRAW_DIR="libraw"
+
+LIBGPHOTO_URL="https://github.com/gphoto/libgphoto2/releases/download/v2.5.28/libgphoto2-2.5.28.tar.xz"
+LIBGPHOTO_DIR="libgphoto"
+
+LIBFTDI_URL="https://github.com/wjakob/libftdi.git"
+LIBFTDI_DIR="libftdi"
+
+INDILIB_URL="https://github.com/indilib/indi.git"
+INDILIB_DIR="indi"
+
+INDILIB_3P_URL="https://github.com/indilib/indi-3rdparty.git"
+INDILIB_3P_DIR="indi-3rdparty"
 
 G='\033[0;32m'
 R='\033[0;31m'
@@ -80,15 +95,15 @@ function install_dep {
 msg "Installing build essentials and INDILib dependencies"
 install_dep
 
-if [ -d "$LIB_NOVA_DIR" ]; then
-  msg "$LIB_NOVA_DIR already exist skipping cloning"
+if [ -d "$LIBNOVA_DIR" ]; then
+  msg "$LIBNOVA_DIR already exist skipping clone"
 else
-  msg "Cloning $LIB_NOVA_DIR"
-  git clone --depth=1 $LIB_NOVA_URL $LIB_NOVA_DIR
+  msg "Cloning $LIBNOVA_DIR"
+  git clone --depth=1 $LIBNOVA_URL $LIBNOVA_DIR
 fi
 
-msg "Building $LIB_NOVA_DIR"
-configure_and_build "$LIB_NOVA_DIR" "autoconf"
+msg "Building $LIBNOVA_DIR"
+configure_and_build "$LIBNOVA_DIR" "autoconf"
 
 if [ -d "$CFITSIO_DIR" ]; then
   msg "$CFITSIO_DIR already exist skipping download"
@@ -97,21 +112,83 @@ else
   wget $CFITSIO_URL -O cfitsio.tar.gz
   msg "Extracting $CFITSIO_DIR"
   mkdir -p "$CFITSIO_DIR"
-  tar -xvf cfitsio.tar.gz
+  tar -xvf cfitsio.tar.gz -C $CFITSIO_DIR --strip-components 1
   rm cfitsio.tar.gz
 fi
 
 msg "Building $CFITSIO_DIR"
 configure_and_build "$CFITSIO_DIR" "configure"
 
-if [ -d "$INDI_LIB_DIR" ]; then
-  msg "$INDI_LIB_DIR already exist skipping cloning"
+if [ -d "$LIBRAW_DIR" ]; then
+  msg "$LIBRAW_DIR already exist skipping clone"
 else
-  msg "Cloning $INDI_LIB_DIR"
-  git clone --depth=1 $INDI_LIB_URL $INDI_LIB_DIR
-  cd $INDI_LIB_DIR 
+  msg "Cloning $LIBRAW_DIR"
+  git clone --depth=1 $LIBRAW_CMAKE_URL $LIBRAW_CMAKE_DIR
+  git clone --depth=1 $LIBRAW_URL $LIBRAW_DIR
+
+  msg "Coppying $LIBRAW_CMAKE_DIR into $LIBRAW_DIR"
+  cp -a $LIBRAW_CMAKE_DIR/. $LIBRAW_DIR
+  rm -rf $LIBRAW_CMAKE_DIR
+  cd $LIBRAW_DIR
+  git apply ../libraw.patch
+  cd ..
+fi
+
+msg "Building $LIBRAW_DIR"
+configure_and_build "$LIBRAW_DIR" "cmake"
+
+if [ -d "$LIBGPHOTO_DIR" ]; then
+  msg "$LIBGPHOTO_DIR already exist skipping clone"
+else
+  msg "Cloning $LIBGPHOTO_DIR"
+  wget $LIBGPHOTO_URL -O libgphoto.tar.xz
+  mkdir -p $LIBGPHOTO_DIR
+  tar -xvf libgphoto.tar.xz -C $LIBGPHOTO_DIR --strip-components 1
+  rm libgphoto.tar.xz
+fi
+
+msg "Building $LIBGPHOTO_DIR"
+configure_and_build "$LIBGPHOTO_DIR" "configure"
+
+if [ -d "$LIBFTDI_DIR" ]; then
+  msg "$LIBFTDI_DIR already exist skipping clone"
+else
+  msg "Cloning $LIBFTDI_DIR"
+  git clone --depth=1 $LIBFTDI_URL $LIBFTDI_DIR
+fi
+
+msg "Building $LIBFTDI_DIR"
+configure_and_build "$LIBFTDI_DIR" "cmake"
+
+if [ -d "$INDILIB_DIR" ]; then
+  msg "$INDILIB_DIR already exist skipping clone"
+else
+  msg "Cloning $INDILIB_DIR"
+  git clone --depth=1 $INDILIB_URL $INDILIB_DIR
+  cd $INDILIB_DIR 
   git apply ../indi.patch
   cd ..
 fi
 
-configure_and_build "$INDI_LIB_DIR" "cmake"
+msg "Building $INDILIB_DIR"
+configure_and_build "$INDILIB_DIR" "cmake"
+
+if [ -d "$INDILIB_3P_DIR" ]; then
+  msg "$INDILIB_3P_DIR already exist skipping clone"
+else
+  msg "Cloning $INDILIB_3P_DIR"
+  git clone --depth=1 $INDILIB_3P_URL $INDILIB_3P_DIR
+  cd $INDILIB_3P_DIR
+  git apply ../indi-3rdparty.patch
+  cd ..
+fi
+
+cd $INDILIB_3P_DIR
+configure_and_build "libqsi" "cmake"
+configure_and_build "libqhy" "cmake"
+configure_and_build "libfishcamp" "cmake"
+configure_and_build "libapogee" "cmake"
+cd ..
+
+msg "Building $INDILIB_3P_DIR"
+configure_and_build "$INDILIB_3P_DIR" "cmake"
